e6086aec7f8fc9d6b89407119c89567fed8e7e8c
Kamran Ayub <kamran.ayub@gmail.com>
2020-12-18 21:27:41 -0600

    text editor

---
+++ editors/TextEditor.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+  data() {
+    return {
+      editing: false,
+    };
+  },
+  methods: {
+    enableEditing() {
+      this.editing = true;
+    },
+    endEditing() {
+      this.editing = false;
+    },
+  },
+};
+</script>
+
+<template>
+  <input
+    v-if="editing"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    @blur="endEditing"
+  />
+  <span @click="enableEditing" v-else>{{ modelValue }}</span>
+</template>
